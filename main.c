c7ef0a3d1874fee95fb3e8fb1ddaa9ab0908d7b8
Xmoke <xmokeplayz999@gmail.com>
2026-06-13 08:54:41 +0600

    Implement linear search in an array

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+// Usage of array -- linear search through an array filled with numbers
+#include <stdio.h>
 
+int main() {
+    int ara[] = {11, 12, 13, 14, 15};
+    int i, n;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    for (i = 0; i < 5; i++) {
+        if (ara[i] == n) {
+            printf("found");
+            break;
+        }
+    }
+    if (i == 5){
+        printf("not found");
+    }
+    return 0;
+}
