c44cccf790917d87d43882b627ae1b7ae2e92524
Xmoke <xmokeplayz999@gmail.com>
2026-06-18 17:29:30 +0600

    Implement basic calculator functionality in main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+x = float(input("Enter 1st number: "))
+y = float(input("Enter 2nd number: "))
+f = input("Enter function (+, -, *, /): ")
 
+if f == "+":
+    print("Answer:", x + y)
+
+elif f == "-":
+    print("Answer:", x - y)
+
+elif f == "*":
+    print("Answer:", x * y)
+
+elif f == "/":
+    if y != 0:
+        print("Answer:", x / y)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid function")
