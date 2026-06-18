1b3f1e00b143054dc6b9cd113ef6631dffea9018
Xmoke <xmokeplayz999@gmail.com>
2026-06-18 17:28:53 +0600

    Implement basic calculator functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,45 @@
+// calc
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x, y;
+    char f;
+
+    cout << "Enter 1st number: ";
+    cin >> x;
+
+    cout << "Enter 2nd number: ";
+    cin >> y;
+
+    cout << "Enter function (+, -, *, /): ";
+    cin >> f;
+
+    if (f == '+')
+    {
+        cout << "Answer: " << x + y;
+    }
+    else if (f == '-')
+    {
+        cout << "Answer: " << x - y;
+    }
+    else if (f == '*')
+    {
+        cout << "Answer: " << x * y;
+    }
+    else if (f == '/')
+    {
+        if (y != 0)
+            cout << "Answer: " << x / y;
+        else
+            cout << "Cannot divide by zero";
+    }
+    else
+    {
+        cout << "Invalid function";
+    }
+
+    return 0;
+}
