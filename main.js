57599fb4914c6f94149d092a7b2607ac2fc42a44
Xmoke <xmokeplayz999@gmail.com>
2026-06-18 17:29:54 +0600

    Add basic calculator functionality in main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const x = parseFloat(prompt("Enter 1st number:"));
+const y = parseFloat(prompt("Enter 2nd number:"));
+const f = prompt("Enter function (+, -, *, /):");
 
+if (f === "+") {
+    console.log("Answer:", x + y);
+} else if (f === "-") {
+    console.log("Answer:", x - y);
+} else if (f === "*") {
+    console.log("Answer:", x * y);
+} else if (f === "/") {
+    if (y !== 0) {
+        console.log("Answer:", x / y);
+    } else {
+        console.log("Cannot divide by zero");
+    }
+} else {
+    console.log("Invalid function");
+}
